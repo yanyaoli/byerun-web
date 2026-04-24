b4eaadeb2aa15b171b99177c312a0c407f783b7e
yanyao <esc@linux.do>
2026-04-24 16:19:52 +0800

    bugfix(map): 修复地图数据和坐标转换逻辑错位，更新了地图

--- app/src/components/MapPreview.vue
+++ app/src/components/MapPreview.vue
@@ -167,66 +167,8 @@ function parseTrack(rawTrack) {
   return points;
 }
 
-function outOfChina(lng, lat) {
-  return lng < 72.004 || lng > 137.8347 || lat < 0.8293 || lat > 55.8271;
-}
-
-function transformLat(lng, lat) {
-  let ret =
-    -100.0 +
-    2.0 * lng +
-    3.0 * lat +
-    0.2 * lat * lat +
-    0.1 * lng * lat +
-    0.2 * Math.sqrt(Math.abs(lng));
-  ret +=
-    ((20.0 * Math.sin(6.0 * lng * Math.PI) + 20.0 * Math.sin(2.0 * lng * Math.PI)) * 2.0) /
-    3.0;
-  ret +=
-    ((20.0 * Math.sin(lat * Math.PI) + 40.0 * Math.sin((lat / 3.0) * Math.PI)) * 2.0) / 3.0;
-  ret +=
-    ((160.0 * Math.sin((lat / 12.0) * Math.PI) + 320.0 * Math.sin((lat * Math.PI) / 30.0)) * 2.0) /
-    3.0;
-  return ret;
-}
-
-function transformLng(lng, lat) {
-  let ret =
-    300.0 +
-    lng +
-    2.0 * lat +
-    0.1 * lng * lng +
-    0.1 * lng * lat +
-    0.1 * Math.sqrt(Math.abs(lng));
-  ret +=
-    ((20.0 * Math.sin(6.0 * lng * Math.PI) + 20.0 * Math.sin(2.0 * lng * Math.PI)) * 2.0) /
-    3.0;
-  ret +=
-    ((20.0 * Math.sin(lng * Math.PI) + 40.0 * Math.sin((lng / 3.0) * Math.PI)) * 2.0) / 3.0;
-  ret +=
-    ((150.0 * Math.sin((lng / 12.0) * Math.PI) + 300.0 * Math.sin((lng / 30.0) * Math.PI)) * 2.0) /
-    3.0;
-  return ret;
-}
-
-function wgs84ToGcj02(lng, lat) {
-  if (outOfChina(lng, lat)) return [lng, lat];
-  const a = 6378245.0;
-  const ee = 0.00669342162296594323;
-  let dLat = transformLat(lng - 105.0, lat - 35.0);
-  let dLng = transformLng(lng - 105.0, lat - 35.0);
-  const radLat = (lat / 180.0) * Math.PI;
-  let magic = Math.sin(radLat);
-  magic = 1 - ee * magic * magic;
-  const sqrtMagic = Math.sqrt(magic);
-  dLat = (dLat * 180.0) / (((a * (1 - ee)) / (magic * sqrtMagic)) * Math.PI);
-  dLng = (dLng * 180.0) / ((a / sqrtMagic) * Math.cos(radLat) * Math.PI);
-  return [lng + dLng, lat + dLat];
-}
-
 function toLatLng(point) {
-  const [gcjLng, gcjLat] = wgs84ToGcj02(point[0], point[1]);
-  return [gcjLat, gcjLng];
+  return [point[1], point[0]];
 }
 
 function getDisplayTrackPoints(points) {
